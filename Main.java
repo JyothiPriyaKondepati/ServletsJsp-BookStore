d6d7023349f06990000bcc534585fe66c86d12d7
JyothiPriyaKondepati <69138976+JyothiPriyaKondepati@users.noreply.github.com>
2020-11-27 16:55:44 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,15 +1,26 @@
-package com.bookstore.model.dao.user;
+package com.bookstore.model.dao.book;
+
+import java.util.List;
+
+import com.bookstore.model.dao.user.User;
+import com.bookstore.model.dao.user.UserDao;
+import com.bookstore.model.dao.user.UserDaoImpl;
 
 public class Main {
 
 	
-		public static void main(String[] args) {
-			UserDao dao=new UserDaoImpl();
-			
-			User user=new User("ramya", "ramya123", "emp");
-			dao.addUser(user);
-			
-			
-		}
-	}
+	//add 
+	UserDao dao=new UserDaoImpl();
+	
+	Book book=new Book("A12", "java", "john", 900.0);
+	dao.add(book);
+	//update price
+//	dao.update(6, 4000);
+//	System.out.println("done");
+	
+	//del book
+//	dao.delete(10);
+	
+	
+}
 
